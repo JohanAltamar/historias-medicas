e0e901096c1880e83cad831bd52631a8d889a2b5
Johan Altamar <johan.altamar.dev@gmail.com>
2022-07-20 15:13:34 -0500

    Add(-): gender and age fields added

---
+++ constants.ts
@@ -0,0 +1,34 @@
+export const YEARS = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+].map((year) => ({
+  label: year,
+  value: year,
+}));
+
+export const MONTHS = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+].map((year) => ({
+  label: year,
+  value: year,
+}));
--- pages/cyd/index.tsx
+++ pages/cyd/index.tsx
@@ -1,4 +1,10 @@
+// Layouts
 import { MainLayout } from "../../components/templates";
+// Components
+import { RadioButton } from "../../components/Form/RadioButton";
+import { Select } from "../../components/Form/Select";
+// Constants
+import { MONTHS, YEARS } from "../../constants";
 
 const CyDPage = () => {
   return (
@@ -6,6 +12,43 @@ const CyDPage = () => {
       <h1 className="text-3xl font-bold text-center my-5">
         Crecimiento y Desarrollo
       </h1>
+      <form>
+        <fieldset className="my-5">
+          <legend className="text-gray-600">Sexo</legend>
+          <RadioButton
+            label="Masculino"
+            name="sex"
+            id="male"
+            value="masculino"
+          />
+          <RadioButton
+            label="Femenino"
+            name="sex"
+            id="female"
+            value="femenino"
+          />
+        </fieldset>
+        <fieldset>
+          <legend className="text-gray-600">Edad</legend>
+          <div className="flex items-center">
+            <Select
+              selectFirst
+              label="Años"
+              id="age"
+              name="age"
+              options={YEARS}
+            />
+            <span className="mx-2">y</span>
+            <Select
+              selectFirst
+              label="Meses"
+              id="months"
+              name="months"
+              options={MONTHS}
+            />
+          </div>
+        </fieldset>
+      </form>
     </MainLayout>
   );
 };
