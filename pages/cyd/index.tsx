b90e47a776b0311297c99d0af7e86e297b483e78
Johan Altamar <johan.altamar.dev@gmail.com>
2022-07-20 13:44:04 -0500

    Add(-): CyD page added

---
+++ pages/cyd/index.tsx
@@ -0,0 +1,13 @@
+import { MainLayout } from "../../components/templates";
+
+const CyDPage = () => {
+  return (
+    <MainLayout title="Crecimiento y Desarrollo | Historias Médicas">
+      <h1 className="text-3xl font-bold text-center my-5">
+        Crecimiento y Desarrollo
+      </h1>
+    </MainLayout>
+  );
+};
+
+export default CyDPage;
