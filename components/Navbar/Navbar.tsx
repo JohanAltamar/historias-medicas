1891b8d26ddb942ee7e1d8292e31bc56cef4cb96
Johan Altamar <johan.altamar.dev@gmail.com>
2022-07-20 17:48:11 -0500

    Add(-): Navbar sticky added

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import { NavLink } from "./NavLink";
 
 export const Navbar = () => {
   return (
-    <nav className="bg-blue-500 text-gray-100 font-bold text-xl flex justify-between items-center h-14 px-4">
+    <nav className="bg-blue-500 text-gray-100 font-bold text-xl flex justify-between items-center h-14 px-4 sticky top-0 shadow-lg">
       <Link href="/">
         <h1 className="h-full flex items-center py-2 cursor-pointer">
           Historias Médicas
