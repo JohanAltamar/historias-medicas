35391cc850d53f2d00ef5736921750c6d4e2c18e
Johan Altamar <johan.altamar.dev@gmail.com>
2022-07-20 14:10:09 -0500

    Add(-): RadioButton added

---
+++ components/Form/RadioButton/RadioButton.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+
+export interface RadioButtonProps
+  extends React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  label: string;
+}
+export const RadioButton: FC<RadioButtonProps> = ({
+  name,
+  label,
+  value,
+  id,
+  ...props
+}) => {
+  return (
+    <div>
+      <input
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        className="border-gray-300text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-offset-0 focus:ring-indigo-200 focus:ring-opacity-50"
+        {...props}
+      />
+      <label htmlFor={id} className="ml-2">
+        {label}
+      </label>
+    </div>
+  );
+};
