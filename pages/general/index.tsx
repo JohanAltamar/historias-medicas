ccd8bbcc0aecb053cc8efd2b3b31236c41e505c0
Johan Altamar <johan.altamar.dev@gmail.com>
2022-08-01 23:56:07 -0500

    Add(-): general page added

---
+++ pages/general/index.tsx
@@ -0,0 +1,13 @@
+import React, { useCallback, useEffect, useState } from "react";
+// Components
+import { MainLayout } from "../../components/templates";
+
+const GeneralPage = () => {
+  return (
+    <MainLayout title="Conceptos | Historias Médicas">
+      <h1 className="text-3xl font-bold text-center my-5">General</h1>
+    </MainLayout>
+  );
+};
+
+export default GeneralPage;
